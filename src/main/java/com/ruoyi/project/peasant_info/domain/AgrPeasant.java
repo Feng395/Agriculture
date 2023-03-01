b0897786be4905830db480f9b2365c4188f7909e
Chen <1145666543@qq.com>
2023-03-01 19:50:45 +0800

    完成农户管理模块

--- src/main/java/com/ruoyi/project/peasant_info/domain/AgrPeasant.java
+++ src/main/java/com/ruoyi/project/peasant_info/domain/AgrPeasant.java
@@ -35,8 +35,42 @@ public class AgrPeasant extends BaseEntity
     private String peaVillage;
 
     /** 年度 */
-    @Excel(name = "年度")
     private String peaYear;
+
+    /** 与户主关系 */
+    @Excel(name = "与户主关系")
+    private String peaRelation;
+
+    /** 致贫原因 */
+    @Excel(name = "致贫原因")
+    private String peaReason;
+
+    /** 健康状况 */
+    private String peaHealth;
+
+    /** 年龄 */
+    private Long peaAge;
+
+    /** 在校状况 */
+    private String peaStatus;
+
+    /** 民族 */
+    private String peaNation;
+
+    /** 户联系电话 */
+    private String peaTel;
+
+    /** 人员识别时间 */
+    private String peaTime;
+
+    /** 劳动技能 */
+    private String peaAbility;
+
+    /** 文化程度 */
+    private String peaEducation;
+
+    /** 户类型 */
+    private String peaType;
 
     public void setId(Long id)
     {
@@ -92,6 +126,105 @@ public class AgrPeasant extends BaseEntity
     {
         return peaYear;
     }
+    public void setPeaRelation(String peaRelation)
+    {
+        this.peaRelation = peaRelation;
+    }
+
+    public String getPeaRelation()
+    {
+        return peaRelation;
+    }
+    public void setPeaReason(String peaReason)
+    {
+        this.peaReason = peaReason;
+    }
+
+    public String getPeaReason()
+    {
+        return peaReason;
+    }
+    public void setPeaHealth(String peaHealth)
+    {
+        this.peaHealth = peaHealth;
+    }
+
+    public String getPeaHealth()
+    {
+        return peaHealth;
+    }
+    public void setPeaAge(Long peaAge)
+    {
+        this.peaAge = peaAge;
+    }
+
+    public Long getPeaAge()
+    {
+        return peaAge;
+    }
+    public void setPeaStatus(String peaStatus)
+    {
+        this.peaStatus = peaStatus;
+    }
+
+    public String getPeaStatus()
+    {
+        return peaStatus;
+    }
+    public void setPeaNation(String peaNation)
+    {
+        this.peaNation = peaNation;
+    }
+
+    public String getPeaNation()
+    {
+        return peaNation;
+    }
+    public void setPeaTel(String peaTel)
+    {
+        this.peaTel = peaTel;
+    }
+
+    public String getPeaTel()
+    {
+        return peaTel;
+    }
+    public void setPeaTime(String peaTime)
+    {
+        this.peaTime = peaTime;
+    }
+
+    public String getPeaTime()
+    {
+        return peaTime;
+    }
+    public void setPeaAbility(String peaAbility)
+    {
+        this.peaAbility = peaAbility;
+    }
+
+    public String getPeaAbility()
+    {
+        return peaAbility;
+    }
+    public void setPeaEducation(String peaEducation)
+    {
+        this.peaEducation = peaEducation;
+    }
+
+    public String getPeaEducation()
+    {
+        return peaEducation;
+    }
+    public void setPeaType(String peaType)
+    {
+        this.peaType = peaType;
+    }
+
+    public String getPeaType()
+    {
+        return peaType;
+    }
 
     @Override
     public String toString() {
@@ -102,6 +235,17 @@ public class AgrPeasant extends BaseEntity
             .append("peaCard", getPeaCard())
             .append("peaVillage", getPeaVillage())
             .append("peaYear", getPeaYear())
+            .append("peaRelation", getPeaRelation())
+            .append("peaReason", getPeaReason())
+            .append("peaHealth", getPeaHealth())
+            .append("peaAge", getPeaAge())
+            .append("peaStatus", getPeaStatus())
+            .append("peaNation", getPeaNation())
+            .append("peaTel", getPeaTel())
+            .append("peaTime", getPeaTime())
+            .append("peaAbility", getPeaAbility())
+            .append("peaEducation", getPeaEducation())
+            .append("peaType", getPeaType())
             .toString();
     }
 }
